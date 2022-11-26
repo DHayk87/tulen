0ee47c40517ad0d33c4de17b50b284807a0158bc
DHayk87 <97260161+DHayk87@users.noreply.github.com>
2022-11-26 19:41:54 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,39 +18,35 @@ function hide() {
   document.querySelector('.slideNav').style.transform = 'translateX(-250px)'
 }
 
-
-
 // ************************************ slider
 
 let parent = window.matchMedia("(max-width:768px)")
 let el = document.querySelectorAll('.slide')
-// let slideTime = setInterval( nextSlide, 4000)
 let currentSlide = 0
-let interval = true
+var interval 
 let slideTime
 
-function first (){
-  if(parent <= parent.matches){
-    slideTime = setInterval( nextSlide, 4000)
-    console.log(parent , slideTime);
-  }
-}
-first()
-window.addEventListener('load', first())
-parent.onchange = () => {
+parent.addEventListener('change', change )
+window.addEventListener('load',change)
+
+function change() {
+
+
   if (parent.matches) {
     console.log('small')
-    // nextSlide()
+    interval = true
 
     function inter() {
-      if (!interval) {
+      console.log(interval);
+      if (interval) {
+
         interval = setInterval(nextSlide, 4000)
       }
     }
     inter()
   } else {
     console.log('big')
-    // stopInt()
+
 
     function stoped() {
       clearInterval(interval)
@@ -59,14 +55,7 @@ parent.onchange = () => {
     stoped()
   }
 }
-// console.log(slideTime)
 
-// let text = document.querySelector('.sliderText')
-
-
-// function slideTime (){
-//   setInterval( nextSlide, 4000)
-// }
 
 function sliderStyle(s) {
   el[currentSlide].className = ('slide')
@@ -78,30 +67,3 @@ function sliderStyle(s) {
 function nextSlide() {
   sliderStyle(currentSlide + 1)
 }
-
-function stopInt() {
-  clearInterval(slideTime)
-}
-// stopInt()
-
-
-
-
-
-// let imgArr = [
-//   '../image/1.jpg.webp',
-//   '../image/2.jpg.webp',
-//   '../image/3.jpg.webp',
-//   '../image/4.jpg.webp',
-//   '../image/5.jpg.webp'
-// ]
-
-
-// function sliderStyle (s){
-//   el[currentSlide].style.opacity = '0'
-//   currentSlide = (s + el.length) % el.length
-//   el[currentSlide].style.zIndex = '10'
-//   el[currentSlide].style.opacity = '1'
-//   el[currentSlide].style.transition = '1s'
-//   console.log(currentSlide);
-// }
